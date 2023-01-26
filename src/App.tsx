ecd20fa88b113ba139fc30733cf01f82c7d5f191
David Alvarez <devdav@pm.me>
2023-01-27 00:30:49 +0100

    Now able to retrieve Playlists

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const clientId = '8d7301f2986c40cd917eda9af9135fcf'; // Your client id
+    const redirectUri = "http://localhost:3000/"  // Your redirect uri;
+    const scope = 'user-read-private user-read-email';
+    const url = 'https://accounts.spotify.com/authorize';
+    const fullUrl = `${url}?response_type=token&client_id=${encodeURIComponent(clientId)}&scope=${encodeURIComponent(scope)}&redirect_uri=${encodeURIComponent(redirectUri)}`
+
+    const matches = window.location.hash.substring(1).match(/access_token=(.*)&token_type/)
+    const token = (matches?.length ?? 0) > 1 ? matches![1] : null
+
+    const [playlists, setPlaylists] = useState<{name:string}[]>([])
+
+    useEffect(() => {
+        console.log("token", token)
+
+        if (!!token) {
+            axios.get("https://api.spotify.com/v1/me/playlists", {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }).then((res) => setPlaylists(res.data.items))
+        }
+    }, [token])
+
+    return (
+        <div className="App">
+            Welcome <a href={fullUrl}>Connect to Spotify</a><br/><br/>
+
+            {!!playlists.length && <>
+                Your playlists:<br/><br/>
+                <ul style={{textAlign:"left", margin: "auto", width: "200px"}}>
+                    {playlists.map(playlist => <li>{playlist.name}</li>)}
+                </ul>
+            </>}
+        </div>
+    );
 }
 
 export default App;
